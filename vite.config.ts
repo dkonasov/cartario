0b5baa83367926096b5a51718d89c85d822705fd
Konasov Dmitriy <d.konasov@yandex.ru>
2026-05-31 14:57:43 +0200

    ci: base url adjustments

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import { fileURLToPath } from "node:url";
 
 export default defineConfig({
   plugins: [],
+  base: process.env.BASE || "/",
   resolve: {
     alias: {
       "@domain": fileURLToPath(new URL("./src/domain", import.meta.url)),
-      "@application": fileURLToPath(new URL("./src/application", import.meta.url)),
-      "@infrastructure": fileURLToPath(new URL("./src/infrastructure", import.meta.url)),
+      "@application": fileURLToPath(
+        new URL("./src/application", import.meta.url),
+      ),
+      "@infrastructure": fileURLToPath(
+        new URL("./src/infrastructure", import.meta.url),
+      ),
       "@ui": fileURLToPath(new URL("./src/ui", import.meta.url)),
       "@shared": fileURLToPath(new URL("./src/shared", import.meta.url)),
     },
